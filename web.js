3893e9088db4835b8301aa2b2145672e3a79bde4
Mason <mason.mideumlee@gmail.com>
2014-09-13 18:33:39 -0700

    Gruntfile image file name changes commented out

--- web.js
+++ web.js
@@ -1,21 +1,13 @@
-// var gzippo = require('gzippo');
-// var express = require('express');
-// var app = express();
-
-// app.use(express.logger('dev'));
-// app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-// app.listen(process.env.PORT || 5000);
-
-var express = require('express')
+var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/dist'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(request, response) {
   // response.send('Hello World!')
-})
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
