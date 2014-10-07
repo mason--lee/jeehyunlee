8663e7e97bf0af4a038b5a87a1d6b60d90f0a5d9
Mason <mason.mideumlee@gmail.com>
2014-10-07 13:10:04 -0700

    need to fixed the nav icon and make it responsive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-var myApp = angular.module('jeehyunWebApp', ['ngRoute', 'ngAnimate']);
+var myApp = angular.module('jeehyunWebApp', ['ngRoute', 'ngAnimate', 'snap', 'sticky']);
 
 // Configure the routes
 myApp.config(function($routeProvider, $locationProvider) {
@@ -49,11 +49,28 @@ myApp.config(function($routeProvider, $locationProvider) {
 	}
 });
 
+myApp.directive("scroll", function($window) {
+	// var wrapper = angular.element(".snap-content"),
+	// 	sticky = angular.element("#sticky"),
+	// 	length = wrapper.height() - sticky.height() + wrapper.offset().top();
+	// var wrapper = angular.element($window);
+	return function(scope, element, attrs) {
+		angular.element($window).bind("scroll", function() {
+			console.log("scrolling!");
+
+			scope.$apply();
+		});
+	}
+})
+
+
+
 myApp.controller('mainController', function($scope) {
 	$scope.pageClass = "page-home";
 });
 
 myApp.controller('aboutController', function($scope) {
+	$scope.pageClass = "page-about";
 	$scope.profile = [
 		{
 			'title': 'Fashion Designer Jeehyun Lee',
@@ -61,12 +78,28 @@ myApp.controller('aboutController', function($scope) {
 			'alt': 'Jeehyun Lee Profile Picture'
 		}
 	];
-
-	$scope.pageClass = "page-about";
 });
 
 myApp.controller('projectController', function($scope) {
-	$scope.pageClass = "page-about";
+	$scope.pageClass = "page-project";
+	$scope.collections = [
+							{shot: 'project-1', count: 1},
+							{shot: 'project-2', count: 2},
+							{shot: 'project-3', count: 3},
+							{shot: 'project-4', count: 4},
+							{shot: 'project-5', count: 5},
+							{shot: 'project-6', count: 6},
+							{shot: 'project-7', count: 7},
+							{shot: 'project-8', count: 8},
+							{shot: 'project-9', count: 9},
+							{shot: 'project-10', count: 10},
+							{shot: 'project-11', count: 11},
+							{shot: 'project-12', count: 12},
+							{shot: 'project-13', count: 13},
+							{shot: 'project-14', count: 14},
+							{shot: 'project-15', count: 15},
+							{shot: 'project-16', count: 16}
+						];
 });
 
 myApp.controller('contactController', function($scope) {
